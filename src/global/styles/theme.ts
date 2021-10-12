1bda7338611da6e2e7a306a5929922214b1960c0
Denison Ferreira <denisonferreira@MacBook-Pro-de-Denison.local>
2021-10-11 23:51:28 -0300

    first commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+  colors: {
+    PRIMARY: '#5636d3',
+
+    SECONDARY: '#ff872c',
+    SECONDARY_LIGHT: 'rgba(255,135,44, 0.3)',
+
+    SUCCESS: '#12a454',
+    SUCCESS_LIGHT: 'rgba(18, 164, 84, .5)',
+
+    ATTENTION: '#e83f5b',
+    ATTENTION_LIGHT: 'rgba(232, 63, 91, .5)',
+
+    SHAPE: "#fff",
+    TITLE: '#363f5f',
+    TEXT: '#969cb2',
+    BACKGROUND: '#f0f2f5',
+  },
+  fonts: {
+    REGULAR: 'Poppins_400Regular',
+    MEDIUM: 'Poppins_500Medium',
+    BOLD: 'Poppins_700Bold',
+  }
+}
